ad1732c104c8bac1f3ad66a914927e402d5dd826
Angelo Zardini <132851904+angelo4o4@users.noreply.github.com>
2026-06-06 17:17:28 +0200

    update zip name

--- scripts/setup_colab.sh
+++ scripts/setup_colab.sh
@@ -3,7 +3,7 @@
 pip install -q -r /content/sixray-kd/requirements.txt
 
 # Dataset extraction
-DATASET_NAME=${1:-"subset.zip"}
+DATASET_NAME=${1:-"subset_clean.zip"}
 DATASET_ZIP_PATH="/content/drive/MyDrive/DatasetAPAI/${DATASET_NAME}"
 DEST="/content/data"
 mkdir -p $DEST
