7a1f50718b74213d951849b70ed4fa2facb0f101
Angelo Zardini <132851904+angelo4o4@users.noreply.github.com>
2026-07-09 23:27:57 +0200

    add vertical flip augmentation

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -8,6 +8,7 @@ class TrainTransform:
 
     def __init__(self, flip_p=0.5, brightness=0.2, contrast=0.2):
         self.flip_p = flip_p
+        self.vflip_p = vflip_p  # adding vertical flip
         self.color_jitter = T.ColorJitter(brightness=brightness, contrast=contrast)
 
     def __call__(self, image, target):
@@ -19,12 +20,19 @@ class TrainTransform:
             for ann in target["annotations"]:
                 x, y, w, h = ann["bbox"]
                 ann["bbox"] = [width - x - w, y, w, h]
+    
+        # vertical flip
+        if random.random() < self.vflip_p:
+            image = T.functional.vflip(image)
+            for ann in target["annotations"]:
+                x, y, w, h = ann["bbox"]
+                ann["bbox"] = [x, height - y - h, w, h]
 
         image = self.color_jitter(image)
         return image, target
 
 
-def build_train_transforms(flip_p=0.5, brightness=0.2, contrast=0.2, enabled=True):
+def build_train_transforms(flip_p=0.5, vflip_p=0.5, brightness=0.2, contrast=0.2, enabled=True):
     if not enabled:
         return None
-    return TrainTransform(flip_p=flip_p, brightness=brightness, contrast=contrast)
+    return TrainTransform(flip_p=flip_p, vflip_p=vflip_p, brightness=brightness, contrast=contrast)
